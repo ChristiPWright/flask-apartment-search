52f9b4bb2e6d95e45a8feeb08d5ca1459f6a8625
ChristiPWright <ChristiPWright@gmail.com>
2025-02-28 15:45:29 -0800

    auth/test_register base tests running, updated test conf file for success

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -18,7 +18,7 @@ def create_app(test_config=None):
     else:
         app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    
+
     db.init_app(app)
     migrate.init_app(app, db)
     
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,8 +1,24 @@
 import pytest
-from flask import g, session
+from flask import g, json, session
 
 from flaskr import db
 from flaskr.models.models import AppUser
+
+def test_register(client, app):
+    with app.app_context():
+        response = client.post(
+            '/auth/register',
+            data=json.dumps({'email': 'a', 'password': 'a'}),  # Convert dict to JSON
+            content_type='application/json' 
+        )
+
+    assert response.json['message'] == 'User registered successfully'
+    assert response.status_code == 201
+
+    #TODO: 2x check db setup/teardown with this psql user's permissions
+    with app.app_context():
+        app_user = AppUser.query.filter_by(email='a').first()
+        assert app_user is not None
 
 #TODO: read docs and understand parametrize 
 # @pytest.mark.parametrize(('username', 'password', 'message'), (
@@ -10,15 +26,3 @@ from flaskr.models.models import AppUser
 #     ('a', '', b'Password is required.'),
 #     ('test', 'test', b'already registered'),
 # ))
-def test_register(client, app):
-    response = client.post(
-        '/auth/register', data={'email': 'a', 'password': 'a'}
-    )
-
-    # assert response.json['message'] == 'User registered successfully'
-    assert response.status_code == 201 #returning 415 right now
-
-    #TODO: 2x check db setup/teardown with this psql user's permissions
-    # with app.app_context():
-    #     app_user = AppUser.query.filter_by(email='a').first()
-    #     assert app_user is not None
