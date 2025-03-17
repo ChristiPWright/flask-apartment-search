6d7da957ef0b5a1f9a982484df3d175cd966b713
ChristiPWright <ChristiPWright@gmail.com>
2025-03-17 14:33:29 -0700

    misc

--- flaskr/rentals.py
+++ flaskr/rentals.py
@@ -7,7 +7,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from flaskr import db
 from flaskr.models.models import Rental, AppUser
-from flaskr.utils.auth import check_user
+from flaskr.utils.auth_util import check_user
 
 bp = Blueprint('rentals', __name__, url_prefix='/')
 @bp.before_request
